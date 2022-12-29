81948800ce8189952452fbd64b823190d4a4574b
Den <denis100703@gmail.com>
2022-12-29 16:30:03 +0300

    Window borderless and viewports test

--- main.cpp
+++ main.cpp
@@ -42,13 +42,15 @@ int main(int, char**) {
   const char* glsl_version = "#version 130";
   glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
   glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
-  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);  // 3.0+ only
 #endif
 
   // Create window with graphics context
   GLFWwindow* window = glfwCreateWindow(
       1280, 720, "Dear ImGui GLFW + OpenGL3 example", NULL, NULL);
   if (window == NULL) return 1;
+
+  // set window borderless
+  // glfwSetWindowAttrib(window, GLFW_DECORATED, GLFW_FALSE);
 
   glfwMakeContextCurrent(window);
   glfwSwapInterval(1);  // Enable vsync
@@ -60,11 +62,23 @@ int main(int, char**) {
   (void)io;
 
   // Set Config Flags
-  io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;  // Enable Keyboard Controls
+  io.ConfigFlags |= ImGuiConfigFlags_NavEnableKeyboard;  // Keyboard Controls
+  io.ConfigFlags |= ImGuiConfigFlags_DockingEnable;      // Keyboard Controls
   io.ConfigDockingWithShift = true;
+
+  // Multi-Viewport Platform Windows
+  // io.ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
 
   // Setup Dear ImGui style (dark)
   ImGui::StyleColorsDark();
+
+  // When viewports are enabled we tweak WindowRounding/WindowBg so platform
+  // windows can look identical to regular ones.
+  ImGuiStyle& style = ImGui::GetStyle();
+  if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+    style.WindowRounding = 0.0f;
+    style.Colors[ImGuiCol_WindowBg].w = 1.0f;
+  }
 
   // Setup Platform/Renderer backends
   ImGui_ImplGlfw_InitForOpenGL(window, true);
@@ -171,6 +185,18 @@ int main(int, char**) {
                  clear_color.z * clear_color.w, clear_color.w);
     glClear(GL_COLOR_BUFFER_BIT);
     ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+    // Update and Render additional Platform Windows
+    // (Platform functions may change the current OpenGL context, so we
+    // save/restore it to make it easier to paste this code elsewhere.
+    // For this specific demo app we could also call
+    // glfwMakeContextCurrent(window) directly)
+    if (io.ConfigFlags & ImGuiConfigFlags_ViewportsEnable) {
+      GLFWwindow* backup_current_context = glfwGetCurrentContext();
+      ImGui::UpdatePlatformWindows();
+      ImGui::RenderPlatformWindowsDefault();
+      glfwMakeContextCurrent(backup_current_context);
+    }
 
     glfwSwapBuffers(window);
   }
