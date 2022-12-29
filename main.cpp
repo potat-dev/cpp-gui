5f88fddc11e81553239e094b4003868f370be1a3
Den <denis100703@gmail.com>
2022-12-29 06:16:27 +0300

    CMake config for opengl, glfw and glad

--- main.cpp
+++ main.cpp
@@ -4,8 +4,8 @@
 // Read online: https://github.com/ocornut/imgui/tree/master/docs
 
 #include "imgui.h"
-#include "backends/imgui_impl_glfw.h"
-#include "backends/imgui_impl_opengl3.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
 #include <stdio.h>
 #define GL_SILENCE_DEPRECATION
 #if defined(IMGUI_IMPL_OPENGL_ES2)
